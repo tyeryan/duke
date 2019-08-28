ad9e609a98f3f738e2caf8dfdcb2d10f01a87e12
Ryan Tay <tay.yee.en@gmail.com>
2019-08-28 23:54:53 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+        userCmd();
+    }
+
+    public static void greeting() {
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void userCmd() {
+        Scanner input = new Scanner(System.in);
+
+        String userCmd = input.nextLine();
+        ArrayList<String> cmdList = new ArrayList<>();
+
+        while (true) {
+            if (userCmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(userCmd);
+            userCmd = input.nextLine();
+            cmdList.add(userCmd);
+        }
     }
 }
